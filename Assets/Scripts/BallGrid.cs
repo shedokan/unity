ece57f38f96f857d6695ce54bd6fdc3d4f8d3979
Eric Shtivelberg <ericsh@remitly.com>
2023-10-13 14:47:07 +0300

    fix: Position balls inside of Canvas

--- Assets/Scripts/BallGrid.cs
+++ Assets/Scripts/BallGrid.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.U2D.Aseprite;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.Serialization;
 
+/*
+ * Holds the ball grid, and places balls in those positions.
+ *
+ * Expect to be a component
+ */
 public class BallGrid : MonoBehaviour {
     private const float Ang60 = 60 * (MathF.PI / 180);
     private static readonly float Sin60 = MathF.Sin(Ang60);
@@ -16,17 +16,16 @@ public class BallGrid : MonoBehaviour {
     /*
      * Config
      */
-    public bool hexagonalPacking = true;
-    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
-    public byte zIndex = 70;
     public GameObject ball;
     public byte ballsPerRow = 10;
     public byte rowCount = 5;
+    public bool hexagonalPacking = true;
 
     /*
      * Private
      */
-    private Vector2 _canvasBottomLeft, _canvasTopRight;
+    private RectTransform _rectTransform;
+    private Rect _rect;
 
     // Calculated using screen size
     private float _ballDiameter;
@@ -36,13 +35,14 @@ public class BallGrid : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         // Validate parameters
-        Assert.IsNotNull(GetComponent<Canvas>());
         Assert.IsNotNull(ball);
+        _rectTransform = GetComponent<RectTransform>();
+        Assert.IsNotNull(_rectTransform);
 
         CalculateScreen();
     }
 
-    private static readonly TimeSpan UpdateEvery = new TimeSpan(0, 0, 1);
+    private static readonly TimeSpan UpdateEvery = new(0, 0, 1);
     private DateTime _lastUpdate;
 
     // Update is called once per frame
@@ -59,25 +59,18 @@ public class BallGrid : MonoBehaviour {
 
     void CalculateScreen() {
         // Fetch the RectTransform from the GameObject
-        RectTransform rectTransform = GetComponent<RectTransform>();
+        _rect = _rectTransform.rect;
 
-        Vector3[] corners = new Vector3[4];
-        // _rectTransform.transform.
-        rectTransform.GetWorldCorners(corners);
-
-        _canvasBottomLeft = corners[0];
-        _canvasTopRight = corners[2];
-
-        Debug.LogFormat("left x: {0}, right x {1}", _canvasBottomLeft.x, _canvasTopRight.x);
+        // Debug.LogFormat("CalculateScreen() - rect: {0}, pivot: {1}", _rect, rectTransform.pivot);
 
         CalculateBallDiameter();
     }
 
     void CalculateBallDiameter() {
         // Resize balls
-        _ballDiameter = (_canvasTopRight.x - _canvasBottomLeft.x) / ballsPerRow;
+        _ballDiameter = _rect.width / ballsPerRow;
         Assert.IsTrue(_ballDiameter > 0);
-        // Debug.LogFormat("ballSize: {0}", _ballDiameter);
+        // Debug.LogFormat("Ball Diameter: {0}", _ballDiameter);
     }
 
 #if UNITY_EDITOR
@@ -89,6 +82,36 @@ public class BallGrid : MonoBehaviour {
     // TODO: Reuse pool, instead of instantiate
     private void CreateBallGrid() {
         // Clear all existing
+        ClearGrid();
+
+        var ballRadius = _ballDiameter / 2;
+        var yOff = hexagonalPacking ? Sin60 * _ballDiameter : _ballDiameter;
+
+        Vector2 pos = new();
+        for(var yPos = 0; yPos < rowCount; yPos++) {
+            pos.y = -ballRadius - yOff * yPos;
+
+            var oddRow = hexagonalPacking && yPos % 2 != 0;
+            var countForRow = ballsPerRow + 1;
+            if(hexagonalPacking && oddRow) {
+                countForRow -= 1;
+            }
+
+            // TODO: Don't recreate rows
+            var row = new List<GameObject>(countForRow);
+            _grid.Add(row);
+            for(byte xPos = 0; xPos < countForRow - 1; xPos++) {
+                pos.x = ballRadius + _ballDiameter * xPos;
+                if(hexagonalPacking && oddRow) {
+                    pos.x += ballRadius;
+                }
+
+                PutBall(ref pos, ref row);
+            }
+        }
+    }
+
+    private void ClearGrid() {
         if(_grid.Count != 0) {
             foreach(var cell in _grid.SelectMany(row => row)) {
                 Destroy(cell);
@@ -96,34 +119,14 @@ public class BallGrid : MonoBehaviour {
 
             _grid.Clear();
         }
+    }
 
-        var ballRadius = _ballDiameter / 2;
+    void PutBall(ref Vector2 pos, ref List<GameObject> row) {
+        // TODO: Use object pooling
+        var newObject = Instantiate(ball, transform);
+        row.Add(newObject);
 
-        var yOff = hexagonalPacking ? Sin60 * _ballDiameter : _ballDiameter;
-
-        Assert.IsTrue(_ballDiameter > 0);
-        for(var yPos = 0; yPos < rowCount; yPos++) {
-            var y = _canvasTopRight.y - ballRadius - yOff * yPos;
-            
-            var oddRow = yPos % 2; // 1 or 0
-            var countForRow = ballsPerRow +1 - (hexagonalPacking ? oddRow : 0);
-
-            // TODO: Don't recreate rows
-            var row = new List<GameObject>(countForRow);
-            _grid.Add(row);
-            for(byte xPos = 0; xPos < countForRow - 1; xPos++) {
-                var x = _canvasBottomLeft.x + ballRadius + _ballDiameter * xPos;
-                if(hexagonalPacking && oddRow > 0) {
-                    x += ballRadius;
-                }
-                
-                // TODO: Use object pooling
-                var pos = new Vector3(x, y, zIndex);
-                var newObject = Instantiate(ball, pos, Quaternion.identity);
-                row.Add(newObject);
-
-                newObject.transform.localScale = new Vector2(_ballDiameter, _ballDiameter);
-            }
-        }
+        newObject.transform.SetLocalPositionAndRotation(pos, Quaternion.identity);
+        newObject.transform.localScale = new Vector2(_ballDiameter, _ballDiameter);
     }
 }
